418d06bbefd500db54180d7c57cc7e3186065955
Ruthtadesse <ruthtadesse4998@gmail.com>
2024-03-01 18:02:45 +0300

    month2_week1 todolist

--- TODOLIST/todolist/src/TodoItem.jsx
+++ TODOLIST/todolist/src/TodoItem.jsx
@@ -10,7 +10,7 @@ const TodoItem = ({ todo, index, completeTodo, removeTodo, showCompleted }) => {
     color: '#000',
     marginRight: '5px',
     boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
-    borderRadius: '5px',
+    borderRadius: '6px',
   };
 
   return (
